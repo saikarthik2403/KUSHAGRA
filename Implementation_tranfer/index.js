def6d8570a7e3654e6f3c2262943d40afabe9f8d
DEVTOWN <123874689+DevTownResources@users.noreply.github.com>
2023-02-24 18:32:54 +0530

    Add files via upload

---
+++ Implementation_tranfer/index.js
@@ -0,0 +1,9 @@
+import {sum} from "./main.js"
+
+import kus from './main.js'
+
+console.log("Index file is working Fine")
+console.log("Welcome to Devtown Web-development class")
+
+console.log(`sum of 2+3 is ${sum()}` )
+console.log(kus)
---
+++ Implementation_tranfer/main.js
@@ -0,0 +1,18 @@
+ let sum = () => {
+    // console.log(`sum of 2+3 is ${2 + 3}`)
+    return 2+3
+}
+
+let sum_val = sum()
+ 
+
+export { sum }
+export default sum_val
+
+
+
+
+// send some data from one js file to another js file we use two keyword
+// Export
+// Import
+
